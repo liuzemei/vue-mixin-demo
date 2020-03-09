4bc5f0a82cd107eb7aa9d6da9e0abdd0d869f52b
liuzemei <her_liu@qq.com>
2020-03-09 10:55:25 +0800

    add return_to

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,16 @@
 import { client_id } from "../keystore.json";
 export default {
   methods: {
-    to_auth() {
+    to_auth(url) {
       window.localStorage.clear();
-      window.location.href = `https://mixin.one/oauth/authorize?client_id=${client_id}&scope=SNAPSHOTS:READ+PROFILE:READ+PHONE:READ+ASSETS:READ+CONTACTS:READ&response_type=code&return_to=/`;
+      url = url || "/";
+      window.location.href = `https://mixin.one/oauth/authorize?client_id=${client_id}&scope=SNAPSHOTS:READ+PROFILE:READ+PHONE:READ+ASSETS:READ+CONTACTS:READ&response_type=code&return_to=${url}`;
     }
   },
   mounted() {
     window._vm = this;
     if (window.location.pathname === "/auth") return;
-    if (!window.localStorage.getItem("token")) this.to_auth();
+    if (!window.localStorage.getItem("token")) this.to_auth(url);
   }
 };
 </script>
